56f8dab995d357df72db574773f35e1d2c9dad44
Daviddsouza-dotcom <ameyrathi2009@gmail.com>
2026-01-15 17:40:38 +0530

    Refactor vibration sending to BluetoothManager1 class
    Refactor vibration sending to use BluetoothManager1 class with send4MotorPattern method.

--- src/lib/bluetooth1.ts
+++ src/lib/bluetooth1.ts
@@ -1,13 +1,26 @@
 // lib/bluetooth1.ts
 
-export async function sendVibration(pattern: number[]) {
-  const response = await fetch("http://127.0.0.1:5000/vibrate", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ s: pattern })
-  });
 
-  if (!response.ok) {
-    throw new Error("LinguaVibe service not running");
+
+export class BluetoothManager1 {
+
+  async send4MotorPattern(motors: Array<{M1: number, M2: number, M3: number, M4: number, duration: number}>): Promise<void> {
+    
+    try {
+      for (const motor of motors) {
+        const jsonData = { s: [motor.M1, motor.M2, motor.M3, motor.M4, motor.duration] };
+        const jsonString = JSON.stringify(jsonData) + '\n';
+        const encoder = new TextEncoder();
+        const data = encoder.encode(jsonString);
+
+        const response = await fetch("http://127.0.0.1:5000/vibrate", {
+           method: "POST",
+           headers: { "Content-Type": "application/json" },
+           body: data });
+	  }
+	} if (!response.ok) {
+      throw new Error("LinguaVibe service not running");
+    }
   }
-}
+
+export const bluetoothManager1 = new BluetoothManager1();
