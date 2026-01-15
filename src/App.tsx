3519f3e01735477a05013f57fcf4e5b1c62055a0
Daviddsouza-dotcom <ameyrathi2009@gmail.com>
2026-01-15 17:38:21 +0530

    Rename TrainPage1 to TrainPage in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { HomePage } from './components/HomePage';
 import { CoursePage } from './components/CoursePage';
 
 
-import { TrainPage1 } from './components/TrainPage1';
+import { TrainPage } from './components/TrainPage';
 
 import { BluetoothPage } from './components/BluetoothPage';
 import { Lesson } from './lib/supabase';
@@ -34,7 +34,7 @@ function App() {
         />
       )}
       {currentPage === 'train' && selectedLesson && (
-        <TrainPage1
+        <TrainPage
           lesson={selectedLesson}
           onNavigateBack={() => setCurrentPage('courses')}
         />
