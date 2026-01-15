80bfcca43786c1737f50a10802e16ec3a6ab1fc2
Daviddsouza-dotcom <ameyrathi2009@gmail.com>
2026-01-15 16:50:16 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { HomePage } from './components/HomePage';
 import { CoursePage } from './components/CoursePage';
 
 
-import { TrainPage } from './components/TrainPage';
+import { TrainPage1 } from './components/TrainPage1';
 
 import { BluetoothPage } from './components/BluetoothPage';
 import { Lesson } from './lib/supabase';
@@ -34,7 +34,7 @@ function App() {
         />
       )}
       {currentPage === 'train' && selectedLesson && (
-        <TrainPage
+        <TrainPage1
           lesson={selectedLesson}
           onNavigateBack={() => setCurrentPage('courses')}
         />
